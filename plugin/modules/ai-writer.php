3f3550000c29da2bc1a4d4e297a70d8878c1f40b
Nabha Cosley <nabha@ananda.org>
2022-09-18 21:24:13 -0700

    Initial commit

---
+++ plugin/modules/ai-writer.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Adds AI-assisted writing tools to post and page editing screens
+ * 
+ * @package Sidekick
+ * @subpackage AI_Writer
+ */
+
---
+++ plugin/sidekick.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Plugin Name:       Sidekick
+ * Plugin URI:        https://forjoyi.live/sidekick/
+ * Description:       Helping website admins get more done, more easily.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Nabha Cosley
+ * Author URI:        https://forjoyi.live/
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://forjoyi.live/sidekick/
+ * Text Domain:       fj-sidekick
+ * Domain Path:       /languages
+ */
+
+define( 'FJ_SIDEKICK_TEXTDOMAIN', 'fj-sidekick' )
+
+/**
+ * Modules
+ */
+include_once( 'modules/ai-writer.php' );
