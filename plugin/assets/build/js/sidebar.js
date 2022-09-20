f0a243a9b8cb11940d2ba81e78266e063aaea4c2
Nabha Cosley <nabha@ananda.org>
2022-09-20 12:23:27 -0700

    Make sidebar functional (but without API key for now)

--- plugin/assets/build/js/sidebar.js
+++ plugin/assets/build/js/sidebar.js
@@ -2,6 +2,16 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "react":
+/*!************************!*\
+  !*** external "React" ***!
+  \************************/
+/***/ (function(module) {
+
+module.exports = window["React"];
+
+/***/ }),
+
 /***/ "@wordpress/components":
 /*!************************************!*\
   !*** external ["wp","components"] ***!
@@ -137,51 +147,127 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__);
 /* harmony import */ var _wordpress_data__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @wordpress/data */ "@wordpress/data");
 /* harmony import */ var _wordpress_data__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_wordpress_data__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_5__);
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, "prototype", { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 
 
 
 
 
-var PluginMetaFields = function PluginMetaFields(props) {
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__.PanelBody, {
-    title: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_2__.__)("AI Writer", "fj-sidekick"),
-    icon: "welcome-write-blog",
-    intialOpen: true
-  }, /*#__PURE__*/React.createElement(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__.TextareaControl, {
-    value: props.prompt_metafield,
-    label: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_2__.__)("Prompt", "fj-sidekick"),
-    onChange: function onChange(value) {
-      return props.onMetaFieldChange(value);
+
+
+var FJSidekickSidebar = /*#__PURE__*/function (_React$Component) {
+  _inherits(FJSidekickSidebar, _React$Component);
+
+  var _super = _createSuper(FJSidekickSidebar);
+
+  function FJSidekickSidebar(props) {
+    var _this;
+
+    _classCallCheck(this, FJSidekickSidebar);
+
+    _this = _super.call(this, props);
+
+    _defineProperty(_assertThisInitialized(_this), "onButtonClick", function () {
+      _this.setState({
+        loading: true
+      });
+
+      var requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer '
+        },
+        body: JSON.stringify({
+          "model": "text-davinci-002",
+          "prompt": _this.props.prompt_metafield,
+          "temperature": 0,
+          "max_tokens": 256
+        })
+      };
+      fetch("https://api.openai.com/v1/completions", requestOptions).then(function (response) {
+        return response.json();
+      }).then(function (data) {
+        _this.setState({
+          results: data.choices[0].text.trim(),
+          loading: false
+        });
+      });
+    });
+
+    _this.state = {
+      results: '',
+      loading: false
+    };
+    return _this;
+  }
+
+  _createClass(FJSidekickSidebar, [{
+    key: "render",
+    value: function render() {
+      var _this2 = this;
+
+      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_5___default().createElement((react__WEBPACK_IMPORTED_MODULE_5___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_5___default().createElement(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__.PanelBody, {
+        title: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_2__.__)("AI Writer", "fj-sidekick"),
+        icon: "welcome-write-blog",
+        intialOpen: true
+      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_5___default().createElement(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__.TextareaControl, {
+        value: this.props.prompt_metafield,
+        label: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_2__.__)("Prompt", "fj-sidekick"),
+        onChange: function onChange(value) {
+          return _this2.props.onMetaFieldChange(value);
+        }
+      }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_5___default().createElement(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__.Button, {
+        isPrimary: true,
+        onClick: this.onButtonClick,
+        style: {
+          marginBottom: 20
+        }
+      }, (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_2__.__)("Get Content", "fj-sidekick")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_5___default().createElement(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__.TextareaControl, {
+        value: this.state.results,
+        label: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_2__.__)("Result", "fj-sidekick"),
+        style: {
+          height: 200,
+          backgroundColor: this.state.loading ? 'blue' : 'rgba(0, 0, 0, 0)'
+        }
+      })));
     }
-  }), /*#__PURE__*/React.createElement(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__.Button, {
-    isPrimary: true,
-    onClick: onGetContent,
-    style: {
-      marginBottom: 20
-    }
-  }, (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_2__.__)("Get Content", "fj-sidekick")), /*#__PURE__*/React.createElement(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__.TextareaControl, {
-    value: props.result_metafield,
-    label: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_2__.__)("Result", "fj-sidekick"),
-    style: {
-      height: 200
-    },
-    onChange: function onChange(value) {
-      return props.onMetaFieldChange(value);
-    }
-  })));
-};
+  }]);
 
-var onGetContent = function onGetContent() {
-  alert('test');
-};
+  return FJSidekickSidebar;
+}((react__WEBPACK_IMPORTED_MODULE_5___default().Component));
 
-PluginMetaFields = (0,_wordpress_data__WEBPACK_IMPORTED_MODULE_4__.withSelect)(function (select) {
+FJSidekickSidebar = (0,_wordpress_data__WEBPACK_IMPORTED_MODULE_4__.withSelect)(function (select) {
   return {
     prompt_metafield: select('core/editor').getEditedPostAttribute('meta')['_fj_sidekick_prompt_metafield']
   };
-})(PluginMetaFields);
-PluginMetaFields = (0,_wordpress_data__WEBPACK_IMPORTED_MODULE_4__.withDispatch)(function (dispatch) {
+})(FJSidekickSidebar);
+FJSidekickSidebar = (0,_wordpress_data__WEBPACK_IMPORTED_MODULE_4__.withDispatch)(function (dispatch) {
   return {
     onMetaFieldChange: function onMetaFieldChange(value) {
       dispatch('core/editor').editPost({
@@ -191,16 +277,16 @@ PluginMetaFields = (0,_wordpress_data__WEBPACK_IMPORTED_MODULE_4__.withDispatch)
       });
     }
   };
-})(PluginMetaFields);
+})(FJSidekickSidebar);
 (0,_wordpress_plugins__WEBPACK_IMPORTED_MODULE_0__.registerPlugin)('fj-sidekick-sidebar', {
   icon: 'format-aside',
   render: function render() {
-    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(_wordpress_edit_post__WEBPACK_IMPORTED_MODULE_1__.PluginSidebarMoreMenuItem, {
+    return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_5___default().createElement((react__WEBPACK_IMPORTED_MODULE_5___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_5___default().createElement(_wordpress_edit_post__WEBPACK_IMPORTED_MODULE_1__.PluginSidebarMoreMenuItem, {
       target: "fj-sidekick-sidebar"
-    }, (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_2__.__)('Sidekick', 'fj-sidekick')), /*#__PURE__*/React.createElement(_wordpress_edit_post__WEBPACK_IMPORTED_MODULE_1__.PluginSidebar, {
+    }, (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_2__.__)('Sidekick', 'fj-sidekick')), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_5___default().createElement(_wordpress_edit_post__WEBPACK_IMPORTED_MODULE_1__.PluginSidebar, {
       name: "fj-sidekick-sidebar",
       title: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_2__.__)('Sidekick', 'fj-sidekick')
-    }, /*#__PURE__*/React.createElement(PluginMetaFields, null)));
+    }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_5___default().createElement(FJSidekickSidebar, null)));
   }
 });
 }();
--- plugin/assets/src/js/sidebar.js
+++ plugin/assets/src/js/sidebar.js
@@ -3,52 +3,86 @@ import { PluginSidebar, PluginSidebarMoreMenuItem } from "@wordpress/edit-post";
 import { __ } from "@wordpress/i18n";
 import { PanelBody, TextareaControl, Button, Dropdown } from "@wordpress/components";
 import { withSelect, withDispatch } from "@wordpress/data";
+import React from 'react';
+class FJSidekickSidebar extends React.Component {    
 
-let PluginMetaFields = (props) => {
+    constructor(props) {
+      super(props);
+      this.state = {
+        results: '',
+        loading: false
+        };
+    }
 
-	return (
-		<>
-			<PanelBody
-				title={__("AI Writer", "fj-sidekick")}
-				icon="welcome-write-blog"
-				intialOpen={ true }
-			>
-				<TextareaControl 
-					value={props.prompt_metafield}
-					label={__("Prompt", "fj-sidekick")}
-					onChange={(value) => props.onMetaFieldChange(value)}
-				/>
+    onButtonClick = () => {
 
-                <Button 
-                    isPrimary
-                    onClick={onGetContent}
-                    style={{marginBottom: 20}}>{__("Get Content", "fj-sidekick")}</Button>
+        this.setState({loading: true});
+    
+        const requestOptions = {
+            method: 'POST',
+            headers: { 
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer '
+                    },
+            body: JSON.stringify({"model": "text-davinci-002", "prompt": this.props.prompt_metafield, "temperature": 0, "max_tokens": 256})
+        };
 
-				<TextareaControl 
-					value={props.result_metafield}
-					label={__("Result", "fj-sidekick")}
-                    style={{height: 200}}
-					onChange={(value) => props.onMetaFieldChange(value)}
-				/>
+        fetch("https://api.openai.com/v1/completions", requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    results: data.choices[0].text.trim(),
+                    loading: false
+                });
 
-			</PanelBody>
-		</>
-	)
+            });
+
+    }
+
+    render() {
+        return (
+            <>
+                <PanelBody
+                    title={__("AI Writer", "fj-sidekick")}
+                    icon="welcome-write-blog"
+                    intialOpen={ true }
+                >
+                    <TextareaControl 
+                        value={this.props.prompt_metafield}
+                        label={__("Prompt", "fj-sidekick")}
+                        onChange={(value) => this.props.onMetaFieldChange(value)}
+                    />
+    
+                    <Button 
+                        isPrimary
+                        onClick={this.onButtonClick}
+                        style={{marginBottom: 20}}>{__("Get Content", "fj-sidekick")}</Button>
+    
+                    <TextareaControl 
+                        value={this.state.results}
+                        label={__("Result", "fj-sidekick")}
+                        style={{
+                            height: 200,
+                            backgroundColor: this.state.loading ? 'blue' : 'rgba(0, 0, 0, 0)'
+                        }}
+                    />
+    
+                </PanelBody>
+            </>
+        );
+    }
+	
 }
 
-const onGetContent = () => {
-    alert('test');
-}
-
-PluginMetaFields = withSelect(
+FJSidekickSidebar = withSelect(
 	(select) => {
 		return {
 			prompt_metafield: select('core/editor').getEditedPostAttribute('meta')['_fj_sidekick_prompt_metafield']
 		}
 	}
-)(PluginMetaFields);
+)(FJSidekickSidebar);
 
-PluginMetaFields = withDispatch(
+FJSidekickSidebar = withDispatch(
 	(dispatch) => {
 		return {
 			onMetaFieldChange: (value) => {
@@ -56,7 +90,7 @@ PluginMetaFields = withDispatch(
 			}
 		}
 	}
-)(PluginMetaFields);
+)(FJSidekickSidebar);
 
 registerPlugin( 'fj-sidekick-sidebar', {
 	icon: 'format-aside',
@@ -72,7 +106,7 @@ registerPlugin( 'fj-sidekick-sidebar', {
 					name="fj-sidekick-sidebar"
 					title={__('Sidekick', 'fj-sidekick')}
 				>
-					<PluginMetaFields />
+					<FJSidekickSidebar />
 				</PluginSidebar>
 			</>
 		)
